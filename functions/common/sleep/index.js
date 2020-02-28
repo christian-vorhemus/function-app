f376cff20f24ed8b799f6d8cb8b7b9c0722bf6eb
Christian Vorhemus <chvorhem@microsoft.com>
2020-02-28 12:09:23 +0100

    Remove function folder

--- functions/common/sleep/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Time {
-    static sleep(milliseconds) {
-        return new Promise(resolve => setTimeout(resolve, milliseconds));
-    }
-}
-exports.Time = Time;
-//# sourceMappingURL=index.js.map
--- functions/common/sleep/index.ts
+++
@@ -1,7 +0,0 @@
-export abstract class Time {
-
-    public static sleep(milliseconds: number) {
-        return new Promise(resolve => setTimeout(resolve, milliseconds));
-    }
-
-}
